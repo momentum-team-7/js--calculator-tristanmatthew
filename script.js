454bb9c530c85e26b7b32f32bab2ddd7af8b4105
tristanmatthew <tristanmatthew@gmail.com>
2021-01-27 22:24:56 -0500

    html, css done, start of JS

---
+++ script.js
@@ -0,0 +1,12 @@
+// let seven = document.querySelector("#button7")
+// seven.addEventListener('click', event => {
+//     console.log(seven)
+// })
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const clearButton = document.querySelector('[data-clear]')
+const resultTextElement = document.querySelector('[result]')
+
+
